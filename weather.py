44ed7aac172db6ff09b0387e040f2fd3441440da
TOAGGA <108759992+TOAGGA@users.noreply.github.com>
2022-07-17 10:00:47 -0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -13,4 +13,11 @@ if reponse.ok:
     data = data.replace("<p>", "")
     data = data.replace("</p>", "")
     print(data)
+
+# don't paste the stuff below yet, wait until I tell you to
+
+from ast import literal_eval
+
+data = literal_eval(data)
+print(str(data["weather"][0]["main"]) + "\n" + str(data["main"]["temp"]) + "\n" + data["name"] + " ," + data["sys"]["country"])
   
